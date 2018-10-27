63bbefa6d8a121fe7ef574eac4821cfdcda598b7
Kristoffer Wikström <kristoffer.wikstrm@gmail.com>
2018-10-27 13:48:11 +0200

    New version

--- js/app.js
+++ js/app.js
@@ -1,56 +1,152 @@
-(function(w){
-    var doc = w.document;
 
-    function validate(e){
-        var re = new RegExp(this.dataset['pattern']);
-
-        if(re.test(this.value)){
-            this.classList.remove('is-invalid');
-        } else {
-            this.classList.add('is-invalid');
+var app = new Vue({
+    el: '#app',
+    data: {
+        chosenPreset: null,
+        form: {
+            startDate: '',
+            orgDose: '',
+            unit: '',
+            reduction: '',
+            holdDays: '',
+            quitBelow: '',
+        },
+        form_controls: [
+            [
+                {
+                    label: 'Startdatum:',
+                    type: 'date',
+                    name: 'startDate',
+                    errors: [],
+                },
+                {
+                    label: 'Nuvarande dos:',
+                    type: 'text',
+                    name: 'orgDose'
+                },
+                {
+                    label: 'Mängdenhet:',
+                    type: 'select',
+                    name: 'unit',
+                    options: [
+                        {label: 'mg', value:'mg'},
+                        {label: 'ml', value:'ml'},
+                        {label: 'st', value:'st'},
+                    ]
+                },
+            ],
+            [
+                {
+                    label: 'Minskning i procent:',
+                    type: 'text',
+                    name: 'reduction'
+                },
+                {
+                    label: 'Dagar per steg:',
+                    type: 'text',
+                    name: 'holdDays',
+                },
+                {
+                    label: 'Sluta när dosen är under:',
+                    type: 'text',
+                    name: 'quitBelow'
+                },
+            ]
+        ],
+        presets:[
+            {name: 'Sertralin', unit: 'mg', doses: [25, 50, 100]},
+            {name: 'Venlafaxin', unit: 'mg', doses: [37.5, 75, 150]}
+        ]
+    },
+    filters:{
+        formatDate: function(d){
+            return d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+        },
+        rounded: function(v){
+            return Math.round(v * 100) / 100;
         }
+    },
+    computed:{
+        presetList: function(){
+            var result = [];
+            var reductionRates = [5, 10];
 
-    }
+            this.presets.forEach(function(p){
+                p.doses.forEach(function(d){
+                    reductionRates.forEach(function(rr){
+                        result.push({ name: p.name, unit: p.unit, dose: d, reduction: rr });
+                    });
+                });
+            });
 
-    function createRow(date, dose){
-        var r = doc.createElement('tr');
-        r.innerHTML = '<td>'+ date.toLocaleDateString('sv') +'</td><td>'+ Math.round(dose * 100) / 100 +'</td>';
-        return r;
-    }
+            return result;
+        },
+        result: function(){
+            var res = [];
+            var startDate = new Date(this.form['startDate']);
+            var orgDose = new Number(this.form['orgDose']);
+            var unit = new Number(this.form['unit']);
+            var reduction = new Number(this.form['reduction']);
+            var holdDays = new Number(this.form['holdDays']);
+            var quitBelow = new Number(this.form['quitBelow']);
 
-    function calc(ev){
-        ev.preventDefault();
-        var startdatum = new Date(Date.parse(doc.getElementById('startdatum').value));
-        var startdos = parseFloat(doc.getElementById('startdos').value);
-        var reduktion = parseFloat(doc.getElementById('reduktion').value) / 100.0;
-        var antal_dagar = parseFloat(doc.getElementById('dagar').value);
-        var slutdos = parseFloat(doc.getElementById('slutdos').value);
+            if(!isNaN(startDate) && orgDose > 0 && unit != null && reduction > 0 && holdDays > 0 && quitBelow > 0){
+                var currentDose = orgDose;
+                var currentDate = new Date(startDate);
+                var step = 1;
 
-        var resultsElm = doc.getElementById('results');
-        resultsElm.innerHTML = '';
+                while(currentDose > quitBelow){
+                    currentDose = orgDose * Math.pow((1.0 - reduction / 100), step);
+                    step = step + 1;
+    
+                    res.push({dose: currentDose, date: currentDate});
 
-        for(var i = 0; i < 3650; i++){
-            var dose = startdos * Math.pow(1 - reduktion, i + 1);
-
-            if(dose < slutdos){
-                break;
+                    var tempDate = new Date(currentDate);
+                    tempDate.setDate(tempDate.getDate() + holdDays);
+                    currentDate = tempDate;
+                }
             }
 
-            var date = new Date(startdatum);
-            date.setDate(startdatum.getDate() + antal_dagar * i);
-
-            resultsElm.appendChild(createRow(date, dose));
-
+            return res;
         }
+    },
+    methods:{
+        getControlByName: function(name){
+            for(var rowIndex = 0; rowIndex < this.form_controls.length; rowIndex++){
+                var row = this.form_controls[rowIndex];
 
-        return false;
+                for(var ctrlIndex = 0; ctrlIndex < row.length; ctrlIndex++){
+                    if(row[ctrlIndex].name == name){
+                        return row[ctrlIndex];
+                    }
+                }
+            }
+
+            return null;
+        },
+        formValChanged: function(e){
+            var ctrl = this.getControlByName(e.target.name);
+            var self = this;
+            var errors = [];
+
+            if(ctrl.validators){
+                ctrl.validators.forEach(function(validator){
+                    errors.push(self[validator](e, ctrl));
+                });
+            }
+
+            ctrl.errors = errors;
+        },
+        testValidator: function(){
+            return "not valid";
+        },
+        presetSelect: function(){
+            console.log("asdas");
+            this.form['unit'] = this.chosenPreset.unit;
+            this.form['reduction'] = this.chosenPreset.reduction;
+            this.form['orgDose'] = this.chosenPreset.dose;
+            this.form['holdDays'] = 28;
+            this.form['quitBelow'] = this.chosenPreset.dose / 100;
+        }
     }
-
-    w.onload = function(){
-        [].slice.call(doc.getElementsByTagName('input')).forEach(function(e){
-            e.onchange = validate;
-        });
-        doc.getElementById('berakna').onclick = calc;
-    };
-
-})(window);
+})
